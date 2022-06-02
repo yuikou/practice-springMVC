35d71106e487a3b18b57ff1b06800dbb0cd99749
yuikou <yuikou777@gmail.com>
2022-06-03 00:41:16 +0800

    file upload: resolve duplicate file name issue

--- springMVC-demo4/src/main/java/com/atguigu/mvc/controller/FileController.java
+++ springMVC-demo4/src/main/java/com/atguigu/mvc/controller/FileController.java
@@ -10,10 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.UUID;
 
 @Controller
 public class FileController {
@@ -34,7 +32,7 @@ public class FileController {
 //        創建http header設置 response header
         MultiValueMap<String, String> headers = new HttpHeaders();
 //        設置下載方式(附件 attachment)及文件檔名
-        headers.add("Content-Disposition","attachment;filename=testpic.jpg");
+        headers.add("Content-Disposition", "attachment;filename=testpic.jpg");
 //      創建ResponseEntity, 建構子(response body內容, response Headers,Http狀態碼)
         ResponseEntity<byte[]> re = new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
 //        關閉input stream
@@ -44,9 +42,28 @@ public class FileController {
     }
 
     @RequestMapping("/testUp")
-    public String testUp(MultipartFile photo){
+    public String testUp(MultipartFile photo, HttpSession session) throws IOException {
+//        獲取上傳的文件的文件名
+        String fileName = photo.getOriginalFilename();
+//        取得文件副檔名
+        String fileSuffix = fileName.substring(fileName.lastIndexOf("."));
+//        將UUID作為上傳文件的檔名
+        String uuid = UUID.randomUUID().toString();
+//        將UUUID加上後綴名, 拼接為最終檔名
+        fileName = uuid + fileSuffix;
+//        通過ServletContext獲取到server中"photo"(存放上傳file)目錄的路徑
+        ServletContext context = session.getServletContext();
+        String photoPath = context.getRealPath("photo");
+        File file = new File(photoPath);
 
-        System.out.println(photo.getOriginalFilename());
+//        判斷photoPath所對應路徑之資料夾是否存在
+        if (!file.exists()) {
+            file.mkdir();
+        }
+//        文件最終上傳路徑
+        String finalPath = photoPath + File.separator + fileName;
+        photo.transferTo(new File(finalPath));
+
         return "success";
     }
 }
